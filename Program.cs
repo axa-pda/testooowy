332c4492c60d36a36da600c551e50d6eefcba06e
pdarni88 <p.darnikowski@gmail.com>
2021-01-22 16:35:05 +0100

    commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testooowy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
